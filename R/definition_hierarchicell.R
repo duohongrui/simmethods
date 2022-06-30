062265774b21bf453e124e7ec803cd8ef9ee7c13
duohongrui <duohongrui@cqnu.edu.cn>
2022-06-30 21:55:16 +0800

    add hierarchicell definition function

--- R/definition_hierarchicell.R
+++ R/definition_hierarchicell.R
@@ -11,201 +11,127 @@
 #'
 hierarchicell_method_definition <- function(...){
 
-  muscat_parameters <- parameter_sets(
+  hierarchicell_parameters <- parameter_sets(
     param_reference(
-      id = "x",
-      type = "SingleCellExperiment",
+      id = "expr",
+      type = "dataframe",
       default = NULL,
       process = "estimation",
       force = TRUE,
-      description = "A SingleCellExperiment.",
-      function_name = c("prepSim", "simData")
+      description = "A data.frame that has been output by filter_counts where the unique cell identifier is in column one and the sample identifier is in column two with the remaining columns all being genes.",
+      function_name = "compute_data_summaries"
+    ),
+    param_character(
+      id = "type",
+      default = "Raw",
+      process = "estimation",
+      alternative = c("Raw", "PerMillion", "Norm"),
+      description = "an identifier for the type of data being submitted. If it is raw counts put 'Raw', if it is TPM or some other normalized counts per million then type 'PerMillion' or 'Norm'. The program assumes data is in one of these two formats. Other normalizations (i.e., logs) that have negative values will cause the program to malfunction.",
+      function_name = "compute_data_summaries"
+    ),
+    param_others(
+      id = "data_summaries",
+      type = "SingleCellExperimrnt",
+      description = "An R object that has been output by the package's compute_data_summaries function. No default.",
+      function_name = "simulate_hierarchicell "
+    ),
+    param_others(
+      id = "n_genes",
+      type = "integer",
+      default = "nrow(ref_data)",
+      description = "An integer. The number of genes you would like to simulate for your dataset. Too large of a number may cause memory failure and may slow the simulation down tremendously. We recommend an integer less than 100,000. ",
+      function_name = "simulate_hierarchicell"
     ),
     param_integer(
-      id = "min_count",
+      id = "n_per_group",
       default = 1L,
-      process = "estimation",
-      description = "Used for filtering of genes; only genes with a count > min_count in >= min_cells will be retained.",
-      function_name = "prepSim"
+      description = "An integer. The number of independent samples per case/control group for simulation. Use when 'binary' is specified as the outcome. Creates a balanced design, for unbalanced designs, specify n_cases and n_controls separately. If not specifying a foldchange, the number of cases and controls does not matter. ",
+      function_name = "simulate_hierarchicell"
     ),
     param_integer(
-      id = "min_cell",
-      default = 10L,
-      process = "estimation",
-      description = "Used for filtering of genes; only genes with a count > min_count in >= min_cells will be retained.",
-      function_name = "prepSim"
+      id = "n_cases",
+      default = 1L,
+      description = "An integer. The number of independent control samples for simulation. ",
+      function_name = "simulate_hierarchicell"
     ),
     param_integer(
-      id = "min_genes",
-      default = 100L,
-      process = "estimation",
-      description = "Used for filtering cells; only cells with a count > 0 in >= min_genes will be retained.",
-      function_name = "prepSim"
-    ),
-    param_integer(
-      id = "min_size",
-      default = 100L,
-      process = "estimation",
-      description = "Used for filtering subpopulation-sample combinations; only instances with >= min_size cells will be retained. Specifying min_size = NULL skips this step.",
-      function_name = "prepSim"
+      id = "n_controls",
+      default = 1L,
+      description = "An integer. The number of independent control samples for simulation. ",
+      function_name = "simulate_hierarchicell"
     ),
     param_others(
-      id = "group_keep",
-      type = "character",
-      process = "estimation",
-      description = "Character string; if nlevels(x$group_id) > 1, specifies which group of samples to keep (see details). The default NULL retains samples from levels(x$group_id)[1]; otherwise, if 'colData(x)$group_id' is not specified, all samples will be kept.",
-      function_name = "prepSim"
-    ),
-    param_Boolean(
-      id = "verbose",
-      default = TRUE,
-      process = "estimation",
-      description = "Logical; should information on progress be reported?",
-      function_name = "prepSim"
-    ),
-    param_others(
-      id = "ng",
+      id = "cells_per_control",
       type = "integer",
-      process = "simulation",
-      default = "nrow(x)",
-      description = "Number of genes to simulate. Importantly, for the library sizes computed by prepSim (= exp(x$offset)) to make sense, the number of simulated genes should match with the number of genes in the reference. To simulate a reduced number of genes, e.g. for testing and development purposes, please set force = TRUE.",
-      function_name = "simData"
+      default = "ncol(ref_data)/2",
+      description = "An integer. The mean number of cells per control you would like to simulate. Too large of a number may cause memory failure and may slow the simulation down tremendously. We recommend an integer less than 1,000.",
+      function_name = "simulate_hierarchicell"
     ),
     param_others(
-      id = "nc",
+      id = "cells_per_case",
       type = "integer",
-      process = "simulation",
-      default = "ncol(x)",
-      description = "Number of cells to simulate.",
-      function_name = "simData"
+      default = "ncol(ref_data)/2",
+      description = "An integer. The mean number of cells per control you would like to simulate. Too large of a number may cause memory failure and may slow the simulation down tremendously. We recommend an integer less than 1,000.",
+      function_name = "simulate_hierarchicell"
     ),
-    param_others(
-      id = "ns",
-      type = "integer",
-      process = "simulation",
-      description = "Number of samples to simulate; defaults to as many as available in the reference to avoid duplicated reference samples. Specifically, the number of samples will be set to n = nlevels(x$sample_id) when dd = FALSE, n per group when dd, paired = TRUE, and floor(n/2) per group when dd = TRUE, paired = FALSE. When a larger number samples should be simulated, set force = TRUE.",
-      function_name = "simData"
-    ),
-    param_others(
-      id = "nk",
-      type = "integer",
-      process = "simulation",
-      description = "Number of clusters to simulate; defaults to the number of available reference clusters.",
-      function_name = "simData"
-    ),
-    param_others(
-      id = "probs",
-      type = "list",
-      process = "simulation",
-      description = "A list of length 3 containing probabilities of a cell belonging to each cluster, sample, and group, respectively. List elements must be NULL (equal probabilities) or numeric values in [0, 1] that sum to 1.",
-      function_name = "simData"
-    ),
-    param_Boolean(
-      id = "dd",
-      default = TRUE,
-      process = "simulation",
-      description = "Whether or not to simulate differential distributions; if TRUE, two groups are simulated and ns corresponds to the number of samples per group, else one group with ns samples is simulated.",
-      function_name = "simData"
-    ),
-    param_vector(
-      id = "p_dd",
-      process = "simulation",
-      description = "Numeric vector of length 6. Specifies the probability of a gene being EE, EP, DE, DP, DM, or DB, respectively.",
-      function_name = "simData"
-    ),
-    param_Boolean(
-      id = "paired",
-      default = FALSE,
-      process = "simulation",
-      description = "Logical specifying whether a paired design should be simulated (both groups use the same set of reference samples) or not (reference samples are drawn at random).",
-      function_name = "simData"
+    param_character(
+      id = "ncells_variation_type",
+      default = "Poisson",
+      alternative = c("Poisson", "NB", "Fixed"),
+      description = "Either 'Poisson', 'NB', or 'Fixed'. Allows the number of cells per individual to be fixed at exactly the specified number of cells per individual, vary slightly with a poisson distribution with a lambda equal to the specified number of cells per individual, or a negative binomial with a mean equal to the specified number of cells and dispersion size equal to one.Defaults to 'Poisson'.",
+      function_name = "simulate_hierarchicell"
     ),
     param_numeric(
-      id = "p_ep",
-      default = 0.5,
+      id = "foldchange",
+      default = 2,
       lower = 0,
-      upper = 1,
-      description = "Numeric specifying the proportion of cells to be shifted to a different expression state in one group.",
-      process = "simulation",
-      function_name = "simData"
+      upper = 10,
+      description = "An integer between 1 and 10. The amount of fold change to simulate a difference in expression between case and control groups. The foldchange changes genes in either direction, so a foldchange of 2 would cause the mean expression in cases to be either twice the amount or half the amount for any particular gene. Defaults to 1.",
+      function_name = "simulate_hierarchicell"
     ),
     param_numeric(
-      id = "p_dp",
-      default = 0.3,
-      lower = 0,
-      upper = 1,
-      description = "Numeric specifying the proportion of cells to be shifted to a different expression state in one group.",
-      process = "simulation",
-      function_name = "simData"
-    ),
-    param_numeric(
-      id = "p_dp",
-      default = 0.5,
-      lower = 0,
-      upper = 1,
-      description = "Numeric specifying the proportion of cells to be shifted to a different expression state in one group.",
-      process = "simulation",
-      function_name = "simData"
-    ),
-    param_numeric(
-      id = "p_type",
+      id = "decrease_dropout",
       default = 0,
       lower = 0,
       upper = 1,
-      description = "Numeric. Probability of EE/EP gene being a type-gene. If a gene is of class 'type' in a given cluster, a unique mean will be used for that gene in the respective cluster.",
-      process = "simulation",
-      function_name = "simData"
+      description = "A numeric proportion between 0 and 1. The proportion by which you would like to simulate decreasing the amount of dropout in your data. For example, if you would like to simulate a decrease in the amount of dropout in your data by twenty percent, then 0.2 would be appropriate. This component of the simulation allows the user to adjust the proportion of dropout if they believe future experiments or runs will have improved calling rates (due to improved methods or improved cell viability) and thereby lower dropout rates. Defaults to 0.",
+      function_name = "simulate_hierarchicell"
     ),
     param_numeric(
-      id = "lfc",
-      default = 2,
-      description = "Numeric value to use as mean logFC (logarithm base 2) for DE, DP, DM, and DB type of genes.",
-      process = "simulation",
-      function_name = "simData"
-    ),
-    param_others(
-      id = "rel_lfc",
-      type = "numeric",
-      default = NULL,
-      description = "Numeric vector of relative logFCs for each cluster. Should be of length nlevels(x$cluster_id) with levels(x$cluster_id) as names. Defaults to factor of 1 for all clusters.",
-      process = "simulation",
-      function_name = "simData"
-    ),
-    param_others(
-      id = "phylo_tree",
-      type = "newick tree",
-      default = "c(ifelse(is.null(phylo_tree), 0, 0.1), 3)",
-      description = "Newick tree text representing cluster relations and their relative distance. An explanation of the syntax can be found here. The distance between the nodes, except for the original branch, will be translated in the number of shared genes between the clusters belonging to these nodes (this relation is controlled with phylo_pars). The distance between two clusters is defined as the sum of the branches lengths separating them.",
-      function_name = "simData"
+      id = "alter_dropout_cases",
+      default = 0,
+      lower = 0,
+      upper = 1,
+      description = "A numeric proportion between 0 and 1. The proportion by which you would like to simulate decreasing the amount of dropout between case control groups. For example, if you would like to simulate a decrease in the amount of dropout in your cases by twenty percent, then 0.2 would be appropriate. This component of the simulation allows the user to adjust the proportion of dropout if they believe the stochastic expression of a gene will differ between cases and controls. For a two-part hurdle model, like MAST implements, this will increase your ability to detect differences. Defaults to 0.",
+      function_name = "simulate_hierarchicell"
     ),
     param_Boolean(
-      id = "force",
+      id = "tSNE_plot",
       default = FALSE,
-      process = "simulation",
-      description = "Logical specifying whether to force simulation when ng and/or ns don't match the number of available reference genes and samples, respectively.",
-      function_name = "simData"
+      description = "A TRUE/FALSE statement for the output of a tSNE plot to observe the global behavior of your simulated data. Seurat will need to be installed for this function to properly work. Defaults to FALSE.",
+      function_name = "simulate_hierarchicell"
     )
   )
 
   hierarchicell_method <- method_definition(
     method = "hierarchicell",
     programming = "R",
-    url = "https://github.com/HelenaLC/muscat",
+    url = "https://github.com/kdzimm/hierarchicell",
     authors = authors_definition(
-      first = "Helena L.",
-      last = "Crowell",
-      email = "helena.crowell@uzh.ch",
-      github = "https://github.com/HelenaLC/muscat",
+      first = "Kip D.",
+      last = "Zimmerman",
+      email = "kdzimmer@wakehealth.edu",
+      github = "https://github.com/kdzimm/hierarchicell",
       orcid = NULL
     ),
     manuscript = manuscript_definition(
-      title = "muscat detects subpopulation-specific state transitions from multi-sample multi-condition single-cell transcriptomics data",
-      doi = "10.1038/s41467-020-19894-4",
-      journal = "Nature Communications",
-      date = "2020",
+      title = "Hierarchicell: an R-package for estimating power for tests of differential expression with single-cell data",
+      doi = "10.1186/s12864-021-07635-w",
+      journal = "BMC Genomics",
+      date = "2021",
       peer_review = TRUE
     ),
-    description = "Multi-sample multi-group scRNA-seq analysis tools.")
+    description = "An R package for simulating cell-type specific and hierarchical single-cell expression data.")
 
   list(hierarchicell_method = hierarchicell_method,
        hierarchicell_parameters = hierarchicell_parameters)
