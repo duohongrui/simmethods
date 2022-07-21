2716719bbb7ea124a9aae865becfafdad575ee43
duohongrui <duohongrui@cqnu.edu.cn>
2022-07-21 15:26:49 +0800

    fix powsimR fucntion

--- R/08-powsimR.R
+++ R/08-powsimR.R
@@ -21,6 +21,12 @@
 #' 2. Protocol. Options are "UMI" (default) (e.g. 10X Genomics, CEL-seq2) or "Read" (e.g. Smart-seq2).
 #' 3. Distribution. "NB" (default) for negative binomial or "ZINB" for zero-inflated negative binomial distribution fitting.
 #' 4. Normalisation. "TMM" (default), "MR", "PosCounts", "UQ", "scran", "Linnorm", "SCnorm", "Census", "depth", "none".
+#'
+#' powsimR also provides an another choice to estimate parameters (not neccessary)
+#' via spike-ins. If users want to use this, make sure that the reference data
+#' must contain ERCC spike-in counts. In addtion, users must set dilution.factor and
+#' volume information by `other_prior = list(dilution.factor = xxx, volume = xxx)`.
+#' For more instructions, see `examples`.
 #' @return A list contains the estimated parameters and the results of execution
 #' detection.
 #' @export
@@ -29,9 +35,32 @@
 #'
 #' Github URL: <https://github.com/bvieth/powsimR>
 #'
+#' @examples
+#' ref_data <- simmethods::data
+#'
+#' ## Estimate parameters without ERCC spike-in
+#' estimate_result <- powsimR_estimation(ref_data = ref_data,
+#'                                       other_prior = list(RNAseq = "singlecell",
+#'                                                          Protocol = "UMI",
+#'                                                          Normalisation = "scran"),
+#'                                       verbose = TRUE,
+#'                                       seed = 111)
+#'
+#' ## Estimate parameters with ERCC spike-in
+#' ### Make sure there are ERCC names in reference data
+#' rownames(ref_data)[grep(rownames(ref_data), pattern = "^ERCC")]
+#' ### Users must input the dilution.factor and volume (nanoliter) to determine the ERCC molecules
+#' estimate_result <- powsimR_estimation(ref_data = ref_data,
+#'                                       other_prior = list(RNAseq = "singlecell",
+#'                                                          Protocol = "UMI",
+#'                                                          Normalisation = "scran",
+#'                                                          dilution.factor = 50000,
+#'                                                          volume = 1),
+#'                                       verbose = TRUE,
+#'                                       seed = 111)
 powsimR_estimation <- function(ref_data,
                                verbose = FALSE,
-                               other_prior,
+                               other_prior = NULL,
                                seed){
 
   ##############################################################################
@@ -50,51 +79,40 @@ powsimR_estimation <- function(ref_data,
   }
   # Add ref_data to other_prior
   other_prior[["countData"]] <- ref_data
+  other_prior[["verbose"]] <- verbose
   # Check
   if(is.null(other_prior[["RNAseq"]])){
     other_prior[["RNAseq"]] <- "singlecell"
-    # stop("Please input the parameter of RNAseq: one of 'bulk' and 'singlecell'")
   }
   if(is.null(other_prior[["Protocol"]])){
     other_prior[["Protocol"]] <- "UMI"
-    # stop("Please input the parameter of Protocol: one of 'UMI' and 'Read'")
   }
   # Sub function
   if(is.null(other_prior[["Distribution"]])){
     other_prior[["Distribution"]] <- "NB"
-    # stop("Please input the parameter of Distribution: one of 'NB' and 'ZINB'")
   }
   if(is.null(other_prior[["Normalisation"]])){
     other_prior[["Normalisation"]] <- "TMM"
-    # stop("Please input the parameter of Normalisation: one of 'TMM', 'MR', 'PosCounts', 'UQ', 'scran', 'Linnorm', 'sctransform', 'SCnorm', 'Census', 'depth' and 'none'")
   }
 
-  if(is.null(other_prior[["subfunction"]])){
-    other_prior[["subfunction"]] <- "estimateParam"
-  }
-
+  ## Spike-in
+  if(!is.null(other_prior[["volume"]]) & !is.null(other_prior[["dilution.factor"]])){
+    spikeData <- ref_data[grep(rownames(ref_data), pattern = "^ERCC"), ]
+    concentration <- simmethods::ERCC_info$con_Mix1_attomoles_ul
+    spikeInfo <- data.frame(SpikeID = simmethods::ERCC_info$ERCC_id,
+                            SpikeInput = concentration*10^-18*6.022*10^23*other_prior[["volume"]]/other_prior[["dilution.factor"]],
+                            row.names = simmethods::ERCC_info$ERCC_id)
+    spikeInfo <- spikeInfo[rownames(spikeData), ]
+    other_prior[["spikeData"]] <- spikeData
+    other_prior[["spikeInfo"]] <- spikeInfo
+    spike_in_formals <- simutils::change_parameters(function_expr = "powsimR::estimateSpike",
+                                                    other_prior = other_prior,
+                                                    step = "estimation")
+    spike_in_formals$Normalisation <- "depth"
+  }else spike_in_formals <- NULL
   estimate_formals <- simutils::change_parameters(function_expr = "powsimR::estimateParam",
                                                   other_prior = other_prior,
                                                   step = "estimation")
-  ## Spike-in
-  if(other_prior[["subfunction"]] == "estimateSpike"){
-    if(is.null(other_prior[["spikeData"]])){
-      stop("Please input the parameter of spikeData")
-    }
-    if(is.null(other_prior[["spikeInfo"]])){
-      stop("Please input the parameter of spikeInfo")
-    }
-    if(is.null(other_prior[["Protocol"]])){
-      stop("Please input the parameter of Protocol: one of 'UMI' and 'Read'")
-    }
-    sub_method_formals <- as.list(formals(powsimR::estimateSpike))
-    for(param in names(sub_method_formals)){
-      names_wait_check <- names(other_prior)
-      if(param %in% names_wait_check){
-        sub_method_formals[[param]] <- other_prior[[param]]
-      }
-    }
-  }
 
   ##############################################################################
   ####                            Estimation                                 ###
@@ -111,20 +129,19 @@ powsimR_estimation <- function(ref_data,
     estimate_detection <- peakRAM::peakRAM(
       estimate_result <- do.call(powsimR::estimateParam, estimate_formals)
     )
-    estSpikeRes <- NULL
-    if(other_prior[["subfunction"]] == "estimateSpike"){
-      cat("Estimating parameters using estimateSpike function\n")
-      estSpikeRes <- do.call(powsimR::estimateSpike, sub_method_formals)
-    }
+    if(!is.null(spike_in_formals)){
+      estSpikeRes <- do.call(powsimR::estimateSpike, spike_in_formals)
+    }else estSpikeRes <- NULL
   }, error = function(e){
     as.character(e)
   })
+  estimate_result <- list(estimate_result = estimate_result,
+                          estSpikeRes = estSpikeRes)
   ##############################################################################
   ####                           Ouput                                       ###
   ##############################################################################
   estimate_output <- list(estimate_result = estimate_result,
-                          estimate_detection = estimate_detection,
-                          estSpikeRes = estSpikeRes)
+                          estimate_detection = estimate_detection)
   return(estimate_output)
 }
 
@@ -135,17 +152,107 @@ powsimR_estimation <- function(ref_data,
 #' @param other_prior A list with names of certain parameters. Some methods need
 #' extra parameters to execute the estimation step, so you must input them. In
 #' simulation step, the number of cells, genes, groups, batches, the percent of
-#' DEGs and other variables are usually customed, so before simulating a dataset
-#' you must point it out.
-#' @param return_format A character. Alternatives choices: list, SingleCellExperiment,
-#' Seurat, h5ad
+#' DEGs are usually customed, so before simulating a dataset you must point it out.
+#' See `Details` below for more information.
+#' @param return_format A character. Alternative choices: list, SingleCellExperiment,
+#' Seurat, h5ad. If you select `h5ad`, you will get a path where the .h5ad file saves to.
 #' @param verbose Logical. Whether to return messages or not.
 #' @param seed A random seed.
 #' @importFrom powsimR Setup simulateDE
 #' @importFrom simutils proportionate
-#'
 #' @export
+#' @details
+#' powsimR provides some choices for users to select suitable parameters according
+#' to different normalization methods, and methods for differential expressed analysis.
+#' 1. Normalisation. "TMM" (default), "MR", "PosCounts", "UQ", "scran", "Linnorm", "sctransform", "SCnorm", "Census", "depth".
+#' 2. DEmethod. "T-Test", "edgeR-LRT", "edgeR-QL", "edgeR-zingeR", "edgeR-ZINB-WaVE", "limma-voom", "limma-trend" (default), "DESeq2", "DESeq2-zingeR", "DESeq2-ZINB-WaVE", "ROTS", "baySeq", "NOISeq", "EBSeq", "MAST", "BPSC", "scDD", "DECENT".
 #'
+#' In addtion to simulate datasets with default parameters, users want to simulate
+#' other kinds of datasets, e.g. a counts matrix with 2 or more cell groups. In
+#' powsimR, you can set extra parameters to simulate datasets.
+#'
+#' The customed parameters you can set are below:
+#' 1. nCells. In powsimR, You can directly set `other_prior = list(nCells = 5000)` to simulate 5000 cells.
+#' 2. nGenes. You can directly set `other_prior = list(nGenes = 5000)` to simulate 5000 genes.
+#' 3. de.prob. You can directly set `other_prior = list(de.prob = 0.2)` to simulate DEGs that account for 20 percent of all genes.
+#' 4. prob.group. You can directly set `other_prior = list(prob.group = c(0.4, 0.6))` to assign two proportions of cell groups. Note that the the length of the vector must be **2**.
+#' 5. nBatches. You can not directly set `other_prior = list(nBatches = 3)` to simulate 3 batches. Instead, you should set `other_prior = list(prob.batch = c(0.3, 0.4, 0.3))` to reach the goal.
+#'
+#' For more customed parameters in Splat, please check [powsimR::simulateDE()].
+#' @references
+#' Vieth B, Ziegenhain C, Parekh S, et al. powsimR: power analysis for bulk and single cell RNA-seq experiments[J]. Bioinformatics, 2017, 33(21): 3486-3488. <https://doi.org/10.1093/bioinformatics/btx435>
+#'
+#' Github URL: <https://github.com/bvieth/powsimR>
+#'
+#' @examples
+#' ref_data <- simmethods::data
+#'
+#' ## Estimate parameters with ERCC spike-in
+#' ### Make sure there are ERCC names in reference data
+#' rownames(ref_data)[grep(rownames(ref_data), pattern = "^ERCC")]
+#' ### Users must input the dilution.factor and volume (nanoliter) to determine the ERCC molecules
+#' estimate_result <- powsimR_estimation(ref_data = ref_data,
+#'                                       other_prior = list(RNAseq = "singlecell",
+#'                                                          Protocol = "UMI",
+#'                                                          Normalisation = "scran",
+#'                                                          dilution.factor = 50000,
+#'                                                          volume = 1),
+#'                                       verbose = TRUE,
+#'                                       seed = 111)
+#'
+#' # (1) Simulate 500 cells and 2000 genes
+#' simulate_result <- simmethods::powsimR_simulation(parameters = estimate_result[["estimate_result"]],
+#'                                                   other_prior = list(nCells = 500,
+#'                                                                      nGenes = 2000),
+#'                                                   return_format = "list",
+#'                                                   verbose = TRUE,
+#'                                                   seed = 111)
+#' count_data <- simulate_result[["simulate_result"]][["count_data"]]
+#' dim(count_data)
+#' ## In powsimR, users always get two groups of cells, and the numbers of cells in
+#' ## different groups are the same (default, prob.group = c(0.5, 0.5)).
+#' col_data <- simulate_result[["simulate_result"]][["col_meta"]]
+#' table(col_data$group)
+#' ## row_data
+#' row_data <- simulate_result[["simulate_result"]][["row_meta"]]
+#' table(row_data$de_genes)[2]/2000
+#'
+#' # (2) Simulate two groups with 20% of DEGs and 4 fold change.
+#' simulate_result <- simmethods::powsimR_simulation(parameters = estimate_result[["estimate_result"]],
+#'                                                   other_prior = list(prob.group = c(0.3, 0.7),
+#'                                                                      de.prob = 0.2,
+#'                                                                      fc.group = 4),
+#'                                                   return_format = "list",
+#'                                                   verbose = TRUE,
+#'                                                   seed = 111)
+#' count_data <- simulate_result[["simulate_result"]][["count_data"]]
+#' dim(count_data)
+#' ## cell information
+#' col_data <- simulate_result[["simulate_result"]][["col_meta"]]
+#' table(col_data$group)
+#' ## gene information
+#' row_data <- simulate_result[["simulate_result"]][["row_meta"]]
+#' table(row_data$de_genes)[2]/4000
+#'
+#'
+#' # (3) Simulate two batches (2 fold change)
+#' simulate_result <- simmethods::powsimR_simulation(parameters = estimate_result[["estimate_result"]],
+#'                                                   other_prior = list(prob.batch = c(0.4, 0.6),
+#'                                                                      fc.batch = 2),
+#'                                                   return_format = "list",
+#'                                                   verbose = TRUE,
+#'                                                   seed = 111)
+#' count_data <- simulate_result[["simulate_result"]][["count_data"]]
+#' dim(count_data)
+#' ## cell information
+#' col_data <- simulate_result[["simulate_result"]][["col_meta"]]
+#' table(col_data$group)
+#' table(col_data$batch)
+#' ## gene information
+#' row_data <- simulate_result[["simulate_result"]][["row_meta"]]
+#' head(row_data)
+#' table(row_data$de_fc)
+#' table(row_data$batch_genes)
 powsimR_simulation <- function(parameters,
                                other_prior = NULL,
                                return_format,
@@ -165,12 +272,12 @@ powsimR_simulation <- function(parameters,
   ####                               Check                                   ###
   ##############################################################################
 
-  other_prior[["estParamRes"]] <- parameters
+  other_prior[["estParamRes"]] <- parameters$estimate_result
   other_prior[["setup.seed"]] <- seed
   other_prior[["verbose"]] <- verbose
 
   if(!is.null(parameters[["estSpikeRes"]])){
-    other_prior[["estSpikeRes"]] <- parameters[["estSpikeRes"]]
+    other_prior[["estSpikeRes"]] <- parameters$estSpikeRes
   }
   ##############################################################################
   ####                                Setup                                  ###
@@ -181,7 +288,7 @@ powsimR_simulation <- function(parameters,
 
   ## genes
   if(is.null(other_prior[["nGenes"]])){
-    other_prior[["ngenes"]] <- parameters[["totalG"]]
+    other_prior[["ngenes"]] <- parameters$estimate_result[["totalG"]]
   }else{
     other_prior[["ngenes"]] <- other_prior[["nGenes"]]
   }
@@ -198,12 +305,15 @@ powsimR_simulation <- function(parameters,
     if(sum(prob.group) != 1){
       stop("The sum of prob.group is not euqal to 1")
     }
+    if(length(prob.group) != 2){
+      stop("The length of prob.group is not euqal to 2")
+    }
   }else{
     prob.group <- c(0.5, 0.5)
   }
   ### Assign cells
   if(is.null(other_prior[["nCells"]])){
-    nCells <- parameters[["totalS"]]
+    nCells <- parameters$estimate_result[["totalS"]]
   }else{
     nCells <- other_prior[["nCells"]]
   }
@@ -283,11 +393,9 @@ powsimR_simulation <- function(parameters,
 
   if(is.null(other_prior[["Normalisation"]])){
     other_prior[["Normalisation"]] <- "TMM"
-    # stop("Please input the parameter of Normalisation: one of 'TMM', 'MR', 'PosCounts', 'UQ', 'scran', 'Linnorm', 'sctransform', 'SCnorm', 'Census', and 'depth'")
   }
   if(is.null(other_prior[['DEmethod']])){
     other_prior[['DEmethod']] <- "limma-trend"
-    # stop("Please input the parameter of DEmethod: one of 'T-Test', 'edgeR-LRT', 'edgeR-QL', 'edgeR-zingeR', 'edgeR-ZINB-WaVE', 'limma-voom', 'limma-trend', 'DESeq2', 'DESeq2-zingeR', 'DESeq2-ZINB-WaVE', 'ROTS', 'baySeq', 'NOISeq', 'EBSeq', 'MAST', 'BPSC', 'scDD', 'DECENT'")
   }
   simulate_formals <- simutils::change_parameters(function_expr = "powsimR::simulateDE",
                                                   other_prior = other_prior,
@@ -340,10 +448,8 @@ powsimR_simulation <- function(parameters,
   if(length(simulate_result[["Counts"]]) != 1){
     col_data <- data.frame("cell_name" = colnames(counts),
                            "group" = paste0("Group",
-                                            c(rep(1, other_prior[["n1"]][1]),
-                                              rep(2, other_prior[["n1"]][2]),
-                                              rep(1, other_prior[["n2"]][1]),
-                                              rep(2, other_prior[["n2"]][2]))),
+                                            c(rep(1, sum(other_prior[["n1"]])),
+                                              rep(2, sum(other_prior[["n2"]])))),
                            "batch" = rep(paste0("Batch", 1:nbatches), (other_prior[["n1"]] + other_prior[["n2"]])))
   }else{
     col_data <- data.frame("cell_name" = colnames(counts),
