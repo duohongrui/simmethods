a62485f46e42d62eae006822da225c9d2ac3e9db
duohongrui <duohongrui@cqnu.edu.cn>
2022-09-27 16:44:17 +0800

    add Splat test

--- tests/testthat/test-1-Splat_test.R
+++ tests/testthat/test-1-Splat_test.R
@@ -1,3 +1,41 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data <- scater::mockSCE()
+ref_data <- as.matrix(SingleCellExperiment::counts(data))
+
+## estimation
+estimate_result <- simmethods::Splat_estimation(
+  ref_data = ref_data,
+  verbose = FALSE,
+  seed = 111
+)
+
+## simulation
+other_prior <- list(prob.group = c(0.4, 0.3, 0.3),
+                    batchCells = c(80, 80),
+                    de.prob = 0.2)
+suppressWarnings(
+  simulate_result <- simmethods::Splat_simulation(
+    parameters = estimate_result[["estimate_result"]],
+    other_prior = other_prior,
+    return_format = "list",
+    verbose = FALSE,
+    seed = 111
+  )
+)
+
+test_that("the class of estimation result", {
+  expect_equal(as.character(class(estimate_result$estimate_result)),
+                   "SplatParams")
 })
+
+test_that("colnames of the cell information", {
+  cell_info <- simulate_result$simulate_result$col_meta
+  expect_equal(colnames(cell_info), c("cell_name", "batch", "group"))
+})
+
+test_that("gene information", {
+  gene_info <- simulate_result$simulate_result$row_meta
+  ## proportion of DEGs
+  expect_equal(round(sum(gene_info$de_gene == "yes")/nrow(gene_info), digits = 1),
+               other_prior$de.prob)
+})
+
